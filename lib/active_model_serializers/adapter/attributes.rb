c2847991a3d5039ccb08fe7a02854cf05e146be5
Igor Nikitin <buckster@givemetraffic.com>
2025-11-24 17:49:06 +0300

    Fix frozen hash issue in serializable_hash method
    Duplicate options to avoid modifying the frozen hash in Rails 8.

--- lib/active_model_serializers/adapter/attributes.rb
+++ lib/active_model_serializers/adapter/attributes.rb
@@ -9,7 +9,8 @@ module ActiveModelSerializers
       end
 
       def serializable_hash(options = nil)
-        options = serialization_options(options)
+        # Rails 8 frozen hash fix
+        options = serialization_options(options).dup
         options[:fields] ||= instance_options[:fields]
         serialized_hash = serializer.serializable_hash(instance_options, options, self)
 
